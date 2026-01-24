23d8b9b4b90c57101c79cd3ce4f46c1d0d727dd0
FernandoMulato <juniormulato139@gmail.com>
2026-01-23 20:52:16 -0500

    New design base finished

--- script.js
+++ script.js
@@ -56,12 +56,86 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== CONTACT EMAIL JS ===============*/ 
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message');
 
+/*
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  
+    The code for sending emails is a sample test.
+
+    Create your account at https://www.emailjs.com/ 
+    and follow the instructions in the video and images 
+    to send emails with your account.
+  
+  
+  // serviceID - templateID - #form - publicKey
+  emailjs.sendForm('service_0elobsw', 'template_fceuvek', '#contact-form', 'BfxPyP7mZZG7sDH2M')
+  .then(() => {
+     // Show sent message
+    contactMessage.textContent = 'Message sent successfully ✅';
+
+     // Remove message after five seconds
+    setTimeout(() => {
+      contactMessage.textContent = '';
+    }, 5000);
+    
+     // Clear input fields
+    contactForm.reset();
+  }, () => {
+    // Show error message
+    contactMessage.textContent = 'Message not sent (service error) ❌';
+  });
+};
+*/
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						          : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+// Link the ID of each section (section id="home") to each link (a href="#home") 
+// and activate the link with the class .active-link
+const scrollActive = () => {
+  // We get the position by scrolling down
+  const scrollY = window.scrollY
+
+  sections.forEach(section => {
+    const id = section.id, // id of each section
+          top = section.offsetTop - 50, // Distance from the top edge
+          height = section.offsetHeight, // Element height
+          link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+
+    if(!link) return
+
+    link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal(
+  {
+    origin: 'top',
+    distance: '60px',
+    duration: '2000',
+    // reset: true, // Animations repeat
+  }
+);
+
+sr.reveal('.home__content, .resume__content:nth-child(1), .footer__container');
+sr.reveal('.home__data, .resume__content:nth-child(2)', {delay: 300, origin: 'bottom'});
+
+sr.reveal('.about__content, .contact__content', {origin: 'bottom'});
+sr.reveal('.about__image, .contact__form', {delay: 300});
+
+sr.reveal('.projects__card', {interval: 100});
